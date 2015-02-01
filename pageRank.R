dc11e6d523acf51fd5c1a55f75763a393cd09e2c
Ofir Shalev <ofirshalev1@gmail.com>
2015-02-01 19:56:00 +0800

    initial

---
+++ pageRank.R
@@ -0,0 +1,9 @@
+
+# This matrix represents the connected graph 
+m <- matrix( c(0.5,0.5,0,0.5,0,1,0,0.5,0), nrow=3, ncol=3, byrow = TRUE )
+
+# > m
+# [,1] [,2] [,3]
+# [1,]  0.5  0.5    0
+# [2,]  0.5  0.0    1
+# [3,]  0.0  0.5    0
